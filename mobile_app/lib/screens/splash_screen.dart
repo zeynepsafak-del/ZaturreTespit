0596b8201432fef32b1809d97e14f978ebc0929e
zeynepsafak-del <zeynepsafak@stu.topkapi.edu.tr>
2026-06-18 16:11:09 +0300

    sprint 2 hatalar düzeltildi
    Co-authored-by: orucismail9 <orucismail9@gmail.com>
    
    Co-authored-by: miyaseedaerol <miyaseedaerol@stu.topkapi.edu.tr>
    
    Co-authored-by: zeynepsafak <zeynepsafak@stu.topkapi.edu.tr>

--- mobile_app/lib/screens/splash_screen.dart
+++ mobile_app/lib/screens/splash_screen.dart
@@ -1,30 +1,12 @@
 import 'package:flutter/material.dart';
-import 'login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-
+  const SplashScreen({super.key});
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _navigateToLogin();
-  }
-
-  void _navigateToLogin() async {
-    await Future.delayed(const Duration(seconds: 3));
-    if (mounted) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const LoginScreen()),
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
